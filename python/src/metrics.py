ac7ed903502224755cfd051599eb7b3a6154d16f
Dom <peluso.dominick@gmail.com>
2024-02-14 14:48:37 -0500

    Alter buckets

--- python/src/metrics.py
+++ python/src/metrics.py
@@ -2,7 +2,7 @@ from prometheus_client import Histogram, Counter
 
 # Create a metric to track time spent and requests made.
 ms_buckets = [
-    25, 50, 75, 100, 125 ,150, 175, 200, 225, 250, 275, 300, 325, 350, 375, 400, 425, 450, 475, 500, 600, 
+    5, 10, 25, 50, 75, 100, 125 ,150, 175, 200, 225, 250, 275, 300, 325, 350, 375, 400, 425, 450, 475, 500, 600, 
     700, 800, 900, 1000, 2000, 3000, 4000, 5000, "+Inf"
 ]
 METRIC_TOTAL_REQUEST_TIME = Histogram(
